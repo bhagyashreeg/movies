a99bb4ddf302a6037b4cdc8234b86df2be4a571b
bhagyashree135 <54316127+bhagyashree135@users.noreply.github.com>
2020-02-07 15:42:41 +0530

    Html and css

---
+++ newMovie.js
@@ -0,0 +1,23 @@
+movieName=[
+    {
+        name : "Family",
+        timing : "10AM,2PM,4PM",
+        description : "A Family Guy will take all the responsibilites of the home.",
+        type : "Comedy",
+        tag : "Animation"
+    },
+    {
+        name : "Panda",
+        timing : "10AM,2PM,4PM",
+        description : "A Panda is in black and white color.",
+        type : "Comedy,Drama",
+        tag : "Animation"
+    },
+    {
+        name : "Frozen",
+        timing : "10AM,2PM,4PM",
+        description : "Frozen, It means ice",
+        type : "Drama, Romantic",
+        tag : "Animation"
+    }
+];
